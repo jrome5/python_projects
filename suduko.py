41d155a1452f690a88070e617909f893af4f1649
Jack Rome <jar7@hw.ac.uk>
2019-12-18 19:51:39 +0000

    Refactor code

--- suduko.py
+++ suduko.py
@@ -17,42 +17,17 @@ def fillGrid(grid):
       shuffle(values_list)      
       for value in values_list:
         #Check that this value has not already be used on this row
-        if not(value in grid[row]):
-          #Check that this value has not already be used on this column
-          if not value in (grid[0][col],grid[1][col],grid[2][col],grid[3][col],grid[4][col],grid[5][col],grid[6][col],grid[7][col],grid[8][col]):
-            #Identify which of the 9 squares we are working on
-            square=[]
-            if row<3:
-              if col<3:
-                square=[grid[i][0:3] for i in range(0,3)]
-              elif col<6:
-                square=[grid[i][3:6] for i in range(0,3)]
-              else:  
-                square=[grid[i][6:9] for i in range(0,3)]
-            elif row<6:
-              if col<3:
-                square=[grid[i][0:3] for i in range(3,6)]
-              elif col<6:
-                square=[grid[i][3:6] for i in range(3,6)]
-              else:  
-                square=[grid[i][6:9] for i in range(3,6)]
-            else:
-              if col<3:
-                square=[grid[i][0:3] for i in range(6,9)]
-              elif col<6:
-                square=[grid[i][3:6] for i in range(6,9)]
-              else:  
-                square=[grid[i][6:9] for i in range(6,9)]
-            #Check that this value has not already be used on this 3x3 square
-            if not value in (square[0] + square[1] + square[2]):
-              grid[row][col]=value
-              if checkGridFull(grid):
-                return True
-              else:
-                if fillGrid(grid):
-                  return True
+        if(checkIfSafe(grid, row, col, value)):
+          grid[row][col]=value
+          if checkGridFull(grid):
+            return True
+          else:
+            if fillGrid(grid):
+              return True
       break
   grid[row][col]=0             
+
+def remove
 
 def printGrid(grid):
 	for row in range(0,9):
@@ -76,31 +51,23 @@ def checkGridFull(grid):
   return True 
 
 values_list = [1,2,3,4,5,6,7,8,9]
-grid = []
-grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
-grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
-grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
-grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
-grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
-grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
-grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
-grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
-grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0])
+grid = np.zeros((9,9))
 
-print(values_list)
-fillGrid(grid)
-printGrid(grid)
-def getSquare(grid, row, column):
-	#Identify which of the 9 squares we are working on
-    square=[]
-    c = column % 3
-    if row<3:
-        square=[grid[i][c:c+3] for i in range(0,3)]
-    elif row<6:
-        square=[grid[i][c:c+3] for i in range(0,3)]
-    else:
-        square=[grid[i][c:c+3] for i in range(0,3)]
-    return square
+def checkIfSafe(grid, row, column, value):
+	return (notUsedInRow(grid, row, value) and notUsedInColumn(grid, column, value) and notUsedInSquare(grid, row-row%3, column-column%3, value))
 
-# if __name__ == '__main__':
-# 	main()
+def notUsedInRow(grid, row, value):
+	return not(value in grid[row])
+
+def notUsedInColumn(grid, column, value):
+	return not value in (grid[0][column],grid[1][column],grid[2][column],grid[3][column],grid[4][column],grid[5][column],grid[6][column],grid[7][column],grid[8][column])
+
+def notUsedInSquare(grid, row_start, column_start, value):
+	for i in range(0,3):
+		for j in range(0,3):
+			if(grid[row_start+i][column_start+j] == value):
+				return False
+	return True
+
+if __name__ == '__main__':
+	main()
