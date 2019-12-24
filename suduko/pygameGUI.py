4b8fcfe9ab7dda3856df5aa74248cb9c5faf0b65
unknown <jar7@hw.ac.uk>
2019-12-24 11:56:29 +0000

    Create Grid with pygame

--- suduko/pygameGUI.py
+++ suduko/pygameGUI.py
@@ -1,36 +1,40 @@
 import pygame
 from generator import Suduko
 import time
+
+def generateSoduko():
+	suduko = Suduko(9)
+	suduko_text = suduko.printGridAsList()
+	screen.fill((255, 255, 255))
+	i = 0
+	for row in suduko_text:
+		text = font.render(row, True, black)
+		screen.blit(text,(100, (i*text.get_height()) + 30))
+		i = i + 1
+		pygame.display.update()
+		time.sleep(0.1)
+
 width = 640
 height = 480
 pygame.init()
 screen = pygame.display.set_mode((640, 480))
 clock = pygame.time.Clock()
-done = False
 running = True
 black = [0,0,0]
-font = pygame.font.SysFont(None, 32)
+font = pygame.font.SysFont(None, 48)
 text = font.render("Hello, World", True, black)
+FPS = 30
+screen.fill((255, 255, 255))
+generateSoduko()
+
 
 while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-          running = False
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-          running = False
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_p:
-        	done = False
-    
-    i = 0
-    if(not done):
-    	suduko = Suduko(9)
-    	suduko_text = suduko.printGridAsList()
-    	screen.fill((255, 255, 255))
-    	for row in suduko_text:
-		    text = font.render(row, True, black)
-		    screen.blit(text,(100, (i*text.get_height()) + 30))
-		    i = i + 1
-		    clock.tick(20)
-		    pygame.display.update()
-    	done = True
+		clock.tick(30)
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				running = False
+			if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+				running = False
+			if event.type == pygame.KEYDOWN and event.key == pygame.K_p:
+				generateSoduko()
 pygame.quit()
